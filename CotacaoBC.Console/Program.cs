29d9258cf11635935cd3100ac254c9b167afe43b
Juliano Nicocelli <juliano.nicocelli@karsten.com.br>
2017-09-28 23:02:49 -0300

    Add project files.

---
+++ CotacaoBC.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CotacaoBC.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
